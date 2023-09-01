7adcf710ad18683f79fca02f5538aa947da56024
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2023-09-01 20:22:19 +0900

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,157 @@
+#include "ofApp.h"	
+
+//--------------------------------------------------------------
+Actor::Actor(vector<glm::vec3>& location_list, vector<vector<int>>& next_index_list, vector<int>& destination_list) {
+
+	this->select_index = ofRandom(location_list.size());
+	while (true) {
+
+		auto itr = find(destination_list.begin(), destination_list.end(), this->select_index);
+		if (itr == destination_list.end()) {
+
+			destination_list.push_back(this->select_index);
+			break;
+		}
+
+		this->select_index = (this->select_index + 1) % location_list.size();
+	}
+
+	this->next_index = this->select_index;
+
+	this->color.setHsb(ofMap(location_list[this->select_index].x, -330, 330, 0, 255), 150, 255);
+}
+
+//--------------------------------------------------------------
+void Actor::update(const int& frame_span, vector<glm::vec3>& location_list, vector<vector<int>>& next_index_list, vector<int>& destination_list) {
+
+	if (ofGetFrameNum() % frame_span == 0) {
+
+		auto tmp_index = this->select_index;
+		this->select_index = this->next_index;
+		int retry = next_index_list[this->select_index].size();
+		this->next_index = next_index_list[this->select_index][(int)ofRandom(next_index_list[this->select_index].size())];
+		while (--retry > 0) {
+
+			auto destination_itr = find(destination_list.begin(), destination_list.end(), this->next_index);
+			if (destination_itr == destination_list.end()) {
+
+				if (tmp_index != this->next_index) {
+
+					destination_list.push_back(this->next_index);
+					break;
+				}
+			}
+
+			this->next_index = next_index_list[this->select_index][(this->next_index + 1) % next_index_list[this->select_index].size()];
+		}
+		if (retry <= 0) {
+
+			destination_list.push_back(this->select_index);
+			this->next_index = this->select_index;
+		}
+	}
+
+	auto param = ofGetFrameNum() % frame_span;
+	auto distance = location_list[this->next_index] - location_list[this->select_index];
+	this->location = location_list[this->select_index] + distance / frame_span * param;
+
+	this->log.push_front(this->location);
+	while (this->log.size() > 15) { this->log.pop_back(); }
+}
+
+//--------------------------------------------------------------
+glm::vec3 Actor::getLocation() {
+
+	return this->location;
+}
+
+//--------------------------------------------------------------
+deque<glm::vec3> Actor::getLog() {
+
+	return this->log;
+}
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(30);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(39);
+	ofSetCircleResolution(60);
+
+	auto span = 15;
+	for (int x = -330; x <= 330; x += span) {
+
+		for (int y = -330; y <= 330; y += span) {
+
+			this->location_list.push_back(glm::vec3(x, y, 0));
+		}
+	}
+
+	for (auto& location : this->location_list) {
+
+		vector<int> next_index = vector<int>();
+		int index = -1;
+		for (auto& other : this->location_list) {
+
+			index++;
+			if (location == other) { continue; }
+
+			float distance = glm::distance(location, other);
+			if (distance <= span) {
+
+				next_index.push_back(index);
+			}
+		}
+
+		this->next_index_list.push_back(next_index);
+	}
+
+	for (int i = 0; i < 1800; i++) {
+
+		this->actor_list.push_back(make_unique<Actor>(this->location_list, this->next_index_list, this->destination_list));
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	int frame_span = 3;
+	int prev_index_size = 0;
+
+	if (ofGetFrameNum() % frame_span == 0) {
+
+		prev_index_size = this->destination_list.size();
+	}
+
+	for (auto& actor : this->actor_list) {
+
+		actor->update(frame_span, this->location_list, this->next_index_list, this->destination_list);
+	}
+
+	if (prev_index_size != 0) {
+
+		this->destination_list.erase(this->destination_list.begin(), this->destination_list.begin() + prev_index_size);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	ofTranslate(ofGetWidth() * 0.5, ofGetHeight() * 0.5);
+	
+	for (auto& actor : this->actor_list) {
+
+		ofSetColor(ofColor(actor->color));
+		ofDrawCircle(actor->getLocation(), 5);
+	}
+}
+
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
